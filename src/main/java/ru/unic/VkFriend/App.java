2460145f43148123a9fb482cea05211089a7057b
Supercep <supercep110@gmail.com>
2018-09-08 18:57:45 +0300

    2

---
+++ src/main/java/ru/unic/VkFriend/App.java
@@ -0,0 +1,38 @@
+package ru.unic.VkFriend;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application {
+
+
+    public static void main(String[] args) {
+        Application.launch(App.class, (java.lang.String[])null);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            AnchorPane root = (AnchorPane) FXMLLoader.load(App.class.getResource("/fxml/hello.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("VkFriend");
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
